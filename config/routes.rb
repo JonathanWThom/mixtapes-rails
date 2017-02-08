8cae85bc93f7b232a4fcd6a09f85bab2e1aa5eb3
Jim Padilla and Jonathan Thom <jim+jonathan@>
2017-02-08 09:14:13 -0800

    Routes created

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root :to => 'mixtapes#index'
+
+  resources :mixtapes, :only => [:index, :show] do
+    resources :reviews, :except => [:index]
+  end
+
+  resources :users do
+    resources :mixtapes
+  end
 end
